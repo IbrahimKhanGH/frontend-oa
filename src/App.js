f627f55630dbdabe531979e3e5662a6afd3a23ca
IbrahimKhanGH <iboo11khan@gmail.com>
2023-04-01 11:32:12 -0500

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
 import './App.css';
+import Data from "./oa.json";
+import React, { useMemo } from "react";
+import BarChart from './component/BarChart';
+import { useTable, useSortBy } from 'react-table';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const data = useMemo(() => Data, []);
+    const columns = useMemo(() => [
+      {
+        Header: "Name",
+        accessor: "person.name",
+      },
+      {
+        Header: "Age",
+        accessor: "person.age",
+      },
+      {
+        Header: "Disease",
+        accessor: "person.disease",
+      },
+      {
+        Header: "Duration",
+        accessor: "person.duration_of_suffering",
+      },
+      {
+        Header: "Treatment",
+        accessor: "person.treatment",
+      }
+
+    ], [] );
+
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+      columns,
+      data,
+    }, useSortBy);
+
+    return (
+      <div className="App">
+        <div className='container'>
+          <div className="chart-container=">
+          <div style={{ width: 400 }}>
+            <BarChart chartData={{
+              labels: data.map((data) => data.person.age),
+              datasets: [
+                {
+                  label: "Age",
+                  data: data.map((data) => data.person.age),
+                  backgroundColor: [
+                    "Pink",
+                    "Blue",
+                    "Orange",
+                    "Green",
+                    "Purple",
+                    "Black",
+                    "Yellow",
+                    "Red",
+                    "Indigo",
+                    "Violet",
+
+                  ],
+                  borderColor: "black",
+                  borderWidth: 1,
+                },
+              ],
+            }} />
+            </div>
+          </div>
+          <table {...getTableProps()}>
+            <thead>
+              {headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => (
+                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                      {column.render("Header")}
+                      <span>
+                        {column.isSorted ? (column.isSortedDesc ? '⬇' : '⬆') : ''}
+                      </span>
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {rows.map((row) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map((cell) => (
+                      <td {...cell.getCellProps()}>
+                        {cell.render("Cell")}
+                      </td>
+                    ))}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
 }
 
 export default App;
